0cb5e0515f8c5a93238a7928887dc725d040df06
Robert Silve <robert@silve.net>
2022-04-14 07:42:41 +0200

    fix

--- app/components/NewActivityButton.tsx
+++ app/components/NewActivityButton.tsx
@@ -1,6 +1,6 @@
 import {Link} from "@remix-run/react";
 
-export default (props: {disabled: boolean}) => {
+export default function NewActivityButton(props: {disabled: boolean}) {
     const now = new Date();
     const enabledButton = (<Link to={`/activities/${now.getFullYear()}/${now.getMonth() + 1}/new`}>
         <div className="bg-sky-600 text-stone-50 w-20 h-20 rounded-full justify-center inline-flex items-center">
@@ -10,7 +10,7 @@ export default (props: {disabled: boolean}) => {
     const disabledButton = (<div className="bg-gray-600 text-stone-50 w-20 h-20 rounded-full justify-center inline-flex items-center">
             NEW
         </div>)
-    return <div className="text-center">
+    return <div className="text-center bg-blue-100 pt-3 pb-3">
         {props.disabled ? disabledButton : enabledButton}
     </div>
 }
